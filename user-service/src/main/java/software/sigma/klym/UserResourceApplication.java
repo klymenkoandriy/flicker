d53d9ae021df1f3b39b9c001dbb06868b8c9964b
Andriy Klymenko <andriy.klymenko@sigmaukraine.com>
2018-02-19 00:09:51 +0200

    Add feign-oauth2.

--- user-service/src/main/java/software/sigma/klym/UserResourceApplication.java
+++ user-service/src/main/java/software/sigma/klym/UserResourceApplication.java
@@ -3,10 +3,11 @@ package software.sigma.klym;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
-//@EnableResourceServer
+@EnableResourceServer
 @EnableEurekaClient
 public class UserResourceApplication extends ResourceServerConfigurerAdapter {
 
