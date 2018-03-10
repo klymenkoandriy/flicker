e382251357d80dc61bd5e380b421018efacf2da2
Andriy Klymenko <andriy.klymenko@sigmaukraine.com>
2018-03-10 21:40:34 +0200

    Add CrossOrigin to POST User.

--- user-service/src/main/java/software/sigma/klym/userservice/controller/UserOpenController.java
+++ user-service/src/main/java/software/sigma/klym/userservice/controller/UserOpenController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -55,6 +56,7 @@ public class UserOpenController {
      * @param user user
      * @return saved user
      */
+    @CrossOrigin("*")
     @Validated
     @ApiOperation(value = "Save User", httpMethod = "POST", response = User.class, notes = "Saves new User.")
     @ApiResponses(value = { @ApiResponse(code = 422, message = INFO_ALREADY_EXISTS, response = ApiError.class) })
