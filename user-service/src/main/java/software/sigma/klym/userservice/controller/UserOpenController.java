3c222efaf75c523d09a4319349a286c29c438575
Andriy Klymenko <andriy.klymenko@sigmaukraine.com>
2018-03-12 13:11:35 +0200

    Add CorsFilter into the zuul service.

--- user-service/src/main/java/software/sigma/klym/userservice/controller/UserOpenController.java
+++ user-service/src/main/java/software/sigma/klym/userservice/controller/UserOpenController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -56,7 +55,6 @@ public class UserOpenController {
      * @param user user
      * @return saved user
      */
-    @CrossOrigin("*")
     @Validated
     @ApiOperation(value = "Save User", httpMethod = "POST", response = User.class, notes = "Saves new User.")
     @ApiResponses(value = { @ApiResponse(code = 422, message = INFO_ALREADY_EXISTS, response = ApiError.class) })
